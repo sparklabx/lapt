d79c3bf0a429fb150c5f45ad69a546400edce08a
Binh Phan <ptbinh2003@gmail.com>
2026-09-11 01:35:01 +0700

    Add lapt::system_deps_write (.lapt/system-deps, ADR-0007)

--- lib/state.sh
+++ lib/state.sh
@@ -6,3 +6,9 @@ lapt::version_write() {
     printf 'installed=%s\n' "$(date -u +%Y-%m-%dT%H:%M:%SZ)"
   } > "$opt_dir/.lapt/version"
 }
+
+lapt::system_deps_write() {
+  local opt_dir=$1
+  mkdir -p "$opt_dir/.lapt"
+  cat > "$opt_dir/.lapt/system-deps"
+}
